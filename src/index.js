7049f01fe9e0be8363771e3519ac2f4c655702e8
origuri <dlcksrb7@naver.com>
2023-12-06 17:34:26 +0900

    2023.12.06 - vanilla, redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { legacy_createStore } from "redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const ADD = "add";
+const MINUS = "minus";
+
+// reducer는 내 데이터를 수정하는 함수
+// count = 0은 count의 초기값이 0이라는 의미
+const countModifyReducer = (count = 0, aciton) => {
+  switch (aciton.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
+};
+
+// store를 사용하기 위해서는 reducer라는 함수가 필요함.
+const countStore = legacy_createStore(countModifyReducer);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+// action을 감지하고 action이 될 때 마다 안에 함수가 실행됨.
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  // dispatch 안에는 오브젝트 타입이 들어가야 함, 반드시 key는 type, value는 string 타입
+  countStore.dispatch({ type: ADD });
+};
+
+const handleMinus = () => {
+  countStore.dispatch({ type: MINUS });
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
